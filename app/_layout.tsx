7eef32d2adf10b0e0b090d9b7da65e21f42f2e3b
Wilfredo Diaz <wdiazdev@gmail.com>
2024-11-02 17:08:39 -0400

    "clerk"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo"
 
-export default function RootLayout() {
+const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+const InitialLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" />
     </Stack>
-  );
+  )
+}
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider publishableKey={clerkPublishableKey}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View } from "react-native"
 
 export default function Index() {
   return (
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Dev Client</Text>
     </View>
-  );
+  )
 }
