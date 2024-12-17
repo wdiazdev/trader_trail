3666159944fc7f84bf7f35bd901b578bb5b74ffc
Wilfredo Diaz <wdiazdev@gmail.com>
2024-12-16 23:16:03 -0500

    fetch account call updated

--- client/src/app/(tabs)/home.tsx
+++ client/src/app/(tabs)/home.tsx
@@ -4,6 +4,8 @@ import Text from "@/src/components/Text"
 import agent from "@/src/api/agent"
 import { useAppContext } from "@/src/store/storeContext"
 import { UserAccount } from "@/src/types"
+import Loader from "@/src/components/Loader"
+import dayjs from "dayjs"
 
 export default function Home() {
   const { state } = useAppContext()
@@ -15,8 +17,13 @@ export default function Home() {
       if (state.user?._id) {
         setIsLoading(true)
         try {
-          const response = await agent.Account.getAccounts(state.user._id)
-          if (response.data) setData(response.data)
+          await agent.Account.getAccounts(state.user._id)
+            .then((response) => {
+              setData(response.data)
+            })
+            .catch((error) => {
+              console.log("error:", error)
+            })
         } catch (error) {
           console.error("error:", error)
         } finally {
@@ -27,9 +34,23 @@ export default function Home() {
     fetchData()
   }, [])
 
+  if (isLoading) {
+    return (
+      <Container>
+        <Loader size="large" />
+      </Container>
+    )
+  }
+
   return (
     <Container>
       <Text>Home</Text>
+      {data?.map((account) => {
+        const formattedDate =
+          dayjs(account.createdAt).format("MMMDYY").toUpperCase() +
+          account.accountId.slice(-4).toUpperCase()
+        return <Text key={account.accountId}>{formattedDate}</Text>
+      })}
     </Container>
   )
 }
